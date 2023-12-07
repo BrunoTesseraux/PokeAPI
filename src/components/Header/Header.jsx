54b4a139b7aff263f7777e6c1a005b09d954153d
johann3r <johannerrombach@gmail.comm>
2023-12-07 18:25:37 +0100

    Header Logo Einbindung

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,19 @@
+import "./Header.scss";
+
+import { Link } from "react-router-dom";
+import Nav from "./../Nav/Nav";
+
 const Header = () => {
-  return <></>;
+  return (
+    <>
+      <header>
+        <Link>
+          <img src="./../../../public/pocketmon.svg" alt="Logo Pocketmon" />
+        </Link>
+        <Nav />
+      </header>
+    </>
+  );
 };
 
 export default Header;
