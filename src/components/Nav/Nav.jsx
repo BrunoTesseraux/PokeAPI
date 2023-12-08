dfdf768b521168e760c3a1d6081254eb01444a4e
johann3r <johannerrombach@gmail.comm>
2023-12-08 09:44:59 +0100

    Darkmode ausgeblendet

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,19 +2,19 @@ import DarkMode from "../DarkMode/DarkMode";
 import Searchbar from "../Searchbar/Searchbar";
 import { Link } from "react-router-dom";
 
-const [isDarkMode, setIsDarkMode] = useState(true);
+// const [isDarkMode, setIsDarkMode] = useState(true);
 
-const toggleDarkMode = () => {
-  setIsDarkMode(!isDarkMode);
-};
+// const toggleDarkMode = () => {
+//   setIsDarkMode(!isDarkMode);
+// };
 
 const Nav = () => {
   return (
-    <div className={isDarkMode ? "dark-mode" : "light-mode"}>
+    <div>
       <nav>
         <Link to="/">Home</Link>
         <Searchbar />
-        <DarkMode isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
+        <DarkMode />
       </nav>
     </div>
   );
