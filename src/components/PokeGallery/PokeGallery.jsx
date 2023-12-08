f610815efef4cfd12e89bcda4976ea1d99365a16
YacineYldrm <yacine@mailbox.org>
2023-12-08 12:35:04 +0100

    refactored filter search function. search button component gelöscht

---
+++ src/components/PokeGallery/PokeGallery.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState, useContext } from "react";
+import { PokeDetailsContext } from "../../context/Context";
+import { Link } from "react-router-dom";
+import PokeItem from "../PokeItem/PokeItem";
+import "./PokeGallery.scss";
+
+const PokeGallery = () => {
+
+    const pokeData = useContext(PokeDetailsContext);
+
+    return ( 
+        <section className="grid">
+        {pokeData.pokedetaildata.map((pokeInfos) => {
+        return (
+        <PokeItem
+        key={pokeInfos.id}
+        name={pokeInfos.name}
+        img={pokeInfos.sprites.other.home.front_default}
+        id={pokeInfos.id}
+        index={pokeInfos.game_index}
+        types={pokeInfos.types}
+      />
+        )
+      })} </section>
+     );
+}
+ 
+export default PokeGallery;
